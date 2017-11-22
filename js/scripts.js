fb5d522d27f9c9d6881de5eb96418130131dd296
Mateusz Lawczys <ma6tek@gmail.com>
2017-11-22 01:40:11 +0100

    Add sass, Add tasks, Add content to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var surname = prompt('And your surname !');
+alert('Hello, ' + name + ' ' + surname);
+console.log('Hello, ' + name + surname);
+var phone = prompt('Enter your phone number:');
+alert('You should never do this again, fool! xD');
+console.log('Fool\'s number: ' + phone);
